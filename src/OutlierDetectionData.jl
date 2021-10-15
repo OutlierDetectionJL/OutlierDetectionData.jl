3fcf958c52866210b0a7871d3ffb3e762fc134cc
David Muhr <muhrdavid@gmail.com>
2021-10-15 19:42:39 +0200

    remove unused utils

--- src/OutlierDetectionData.jl
+++ src/OutlierDetectionData.jl
@@ -7,5 +7,5 @@ module OutlierDetectionData
     ELKI.list(prefix::Union{Regex, AbstractString}) = with_prefix(ELKI.list(), prefix)
     ODDS.list(prefix::Union{Regex, AbstractString}) = with_prefix(ODDS.list(), prefix)
 
-    export ODDS, ELKI, with_env, with_accept
+    export ODDS, ELKI
 end
--- src/utils.jl
+++ src/utils.jl
@@ -1,14 +1,5 @@
 using DataDeps
 
-function with_env(f, variable::AbstractString, override::Bool)
-    value = override ? override : get(ENV, variable, false)
-    withenv(f, variable => string(value))
-end
-
-function with_accept(f, override::Bool = true)
-    with_env(f, "DATADEPS_ALWAYS_ACCEPT", override)
-end
-
-function with_prefix(ls::AbstractVector{<:AbstractString}, prefix::Regex)
+function with_prefix(ls::AbstractVector{<:AbstractString}, prefix)
     ls[startswith.(ls, prefix)]
 end
