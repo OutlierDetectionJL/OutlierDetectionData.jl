1110ee540df60fdaa2204f96ac524f0193420e37
David Muhr <muhrdavid@gmail.com>
2021-11-03 16:22:10 +0100

    add documentation for list and load

--- src/OutlierDetectionData.jl
+++ src/OutlierDetectionData.jl
@@ -1,5 +1,6 @@
 module OutlierDetectionData
     include("utils.jl")
+    include("ARFF/ARFF.jl")
     include("ODDS/ODDS.jl")
     include("ELKI/ELKI.jl")
 
@@ -7,5 +8,25 @@ module OutlierDetectionData
     ELKI.list(prefix::Union{Regex, AbstractString}) = with_prefix(ELKI.list(), prefix)
     ODDS.list(prefix::Union{Regex, AbstractString}) = with_prefix(ODDS.list(), prefix)
 
-    export ODDS, ELKI
+    """    list([prefix])
+    List the available datasets in a dataset collection optionally given a prefix.
+
+    Parameters
+    ----------
+        prefix::Union{Regex, AbstractString}
+    Regex or string used to filter the datasets.    
+    """
+    function list end
+
+    """    load(dataset)
+    Load a given dataset from a dataset collection.
+
+    Parameters
+    ----------
+        name::AbstractString
+    Name of the dataset to load.
+    """
+    function load end
+
+    export ODDS, ELKI, ARFF, list, load
 end
